d9b49acd1362e155ac499e2cf3d2f3b1f047d328
Alex Duchesne <alex@netvps.ca>
2020-01-29 22:21:16 -0500

    Disabling SPIRAM_CACHE_WORKAROUND gives 15% performance boost

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #make -j 4
 release=`date +%Y%m%d`;
-../odroid-go-firmware/tools/mkfw/mkfw "Snes9x ($release)" tile.raw 0 16 2097152 app build/snes9x-esp32.bin
+../odroid-go-multi-firmware/tools/mkfw/mkfw "Snes9x ($release)" tile.raw 0 16 2097152 app build/snes9x-esp32.bin
 mv firmware.fw snes9x.fw
